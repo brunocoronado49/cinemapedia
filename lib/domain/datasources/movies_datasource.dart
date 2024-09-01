81b47e6abbb9071c71259c001f1b37fe51ffecc5
brunocoronado49 <francorangelcoronado@gmail.com>
2024-08-31 18:23:00 -0600

    toda la arquitectura bien locochona implementada, alv no entendi ni madres juasjuas

--- lib/domain/datasources/movies_datasource.dart
+++ lib/domain/datasources/movies_datasource.dart
@@ -1,6 +1,6 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
 
 abstract class MoviesDatasource {
-  Future<Movie> getNowPlaying({int page = 1});
+  Future<List<Movie>> getNowPlaying({int page = 1});
 }
 
---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,16 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
+
+class MovieRepositoryImpl extends MoviesRepository {
+  final MoviesDatasource moviesDataSource;
+
+  MovieRepositoryImpl(this.moviesDataSource);
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return moviesDataSource.getNowPlaying(page: page);
+  }
+
+ }
+
