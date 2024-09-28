bcf0245a4a4a286b1031e89f9a59ad5eb0def035
brunocoronado49 <francorangelcoronado@gmail.com>
2024-09-28 11:05:43 -0600

    Dandole logica a la parte de busquedaq

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -16,8 +16,8 @@ class MovieMapper {
     popularity: moviedb.popularity,
     posterPath: moviedb.posterPath != ''
       ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}' 
-      : 'no-poster',
-    releaseDate: moviedb.releaseDate,
+      : 'https://ranobehub.org/img/ranobe/posters/default.jpg',
+    releaseDate: moviedb.releaseDate ?? DateTime.now(),
     title: moviedb.title,
     video: moviedb.video,
     voteAverage: moviedb.voteAverage,
